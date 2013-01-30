464e36d1d1f6d361488caea688893433bceafd16
Salim Kayal <salim.kayal@idiap.ch>
2013-01-30 09:19:36 +0100

    added some support for xmonad files in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,15 @@ rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 ln -s $f/.vimrc
 ln -s $f/.vim
+
+rm -rf $HOME/.xmonad
+mkdir -p $HOME/.xmonad
+pushd $HOME/.xmonad
+ln -s $f/.xmonad/xmonad.hs
+ln -s $f/.xmonad/xmonad.startup
+popd
+xmonad --recompile
+
 cd $f
 
 wget "http://downloads.sourceforge.net/project/pyclewn/pyclewn-1.9/pyclewn-1.9.py2.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpyclewn%2Ffiles%2Flatest%2Fdownload%3Fsource%3Dfiles&ts=1342183882&use_mirror=switch" -O pyclewn-1.9.py2.tar.gz
