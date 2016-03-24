be587b11fdd1662653ca50539e8149f86512a546
Salim Kayal <salim.kayal@idiap.ch>
2016-03-24 17:58:58 +0100

    env

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-#setup vim
 f="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#setup vim
 cd $HOME
 rm -rf $HOME/.vim
-rm -rf $HOME/.vimrc
+rm -f $HOME/.vimrc
 ln -s $f/.vimrc
 ln -s $f/.vim
+
 #setup zsh
+rm -f $HOME/.zshrc
 ln -s $f/.zshrc
+
+#setup xresources for urxvt
+rm -f $HOME/.Xresources
+ln -s $f/.Xresources
+
+#setup remapping for caps-lock
+rm -f $HOME/.Xmodmaprc
+ln -s $f/.Xmodmaprc
 
 #setup xmonad
 rm -rf $HOME/.xmonad
@@ -19,7 +30,25 @@ ln -s $f/.xmonad/xmonad.startup
 popd
 xmonad --recompile
 
+#setup vimperator
+rm -rf $HOME/.vimperator
+rm -rf $HOME/.vimperatorrc
+ln -s $f/.vimperatorrc
+mkdir -p .vimperator/plugin
+mkdir -p .vimperator/color
+pushd .vimperator/plugin
+ln -s $f/noscript.js
+popd
+pushd .vimperator/color
+ln -s $f/molokai.vimp
+popd
+
+actions='#urxvt\nxrdb -merge ~/.Xresources\n#us intl kb\nsetxkbmap -layout us -variant altgr-intl\n#caps_lock remap\nxmodmap ~/.Xmodmaprc' 
+echo -e $actions >> $HOME/.xprofile
+echo -e $actions >> $HOME/.xinitrc
+
 cd $f
 
 #setup vim plugins
 vim +BundleInstall +qall
+
