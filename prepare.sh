5a7ae33177f71dc81e83827cb3350932e76a00c4
Salim Kayal <salim.kayal@idiap.ch>
2026-07-14 11:28:32 +0200

    chore: remove x11 stuff

--- prepare.sh
+++ prepare.sh
@@ -13,21 +13,6 @@ pushd ${HOME}
 rm -f .zshrc
 ln -s $f/.zshrc
 
-#setup remapping for caps-lock
-rm -f .Xmodmaprc
-ln -s $f/.Xmodmaprc
 
-#setup xmonad
-rm -rf .xmonad
-mkdir -p .xmonad
-pushd .xmonad
-ln -s $f/.xmonad/xmonad.hs
-ln -s $f/.xmonad/xmonad.startup
-popd
-xmonad --recompile
-
-actions='#us intl kb\nsetxkbmap -layout us -variant altgr-intl\n#caps_lock remap\nxmodmap ~/.Xmodmaprc' 
-echo -e $actions >> .xprofile
-echo -e $actions >> .xinitrc
 
 popd
